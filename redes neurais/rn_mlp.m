7cc0da7339fc8a052efb589cf80155874d58448c
Alan Silva <alan.silva@gcsec.com.br>
2013-11-11 06:45:50 -0200

    Implementacao Funcao Custo e Backpropagation e adicionado sigmoidGradiente para o backpropagation

--- redes neurais/rn_mlp.m
+++ redes neurais/rn_mlp.m
@@ -15,6 +15,7 @@ tam_cam_entrada  = 137; % quantidade de neuronios na camada de entrada
 tam_cam_saida  = 2;     % quantidade de neuronios na camada de saida
 tam_cam_inter = 274;    % quantidade de neuronios na camada intermediaria
 num_cam = 3;            % quantidades de camadas   
+lambda = 0;
                           
 
 %  Configura a matriz apropriadamente e adiciona uns na primeira coluna
@@ -27,22 +28,58 @@ X = [ones(m, 1) x];
 theta_1 = rand(n + 1, tam_cam_inter);
 theta_2 = rand(tam_cam_inter + 1, tam_cam_saida); 
 
+J = 0;
+Theta1_grad = zeros(size(theta_1));
+Theta2_grad = zeros(size(theta_2));
+
 %X_1 = X * theta_1;
 
 %forward-propagation
 % a1 = X;
-z2 = X * theta_1;
-a2 = sigmoid(z2);
-a2 = [ones(m,1) a2];
-z3 = a2 * theta_2;  
-h_theta = sigmoid(z3); %a3 = h_theta = z3
+%z2 = X * theta_1;
+%a2 = sigmoid(z2);
+%a2 = [ones(m,1) a2];
+%z3 = a2 * theta_2;  
+%h_theta = sigmoid(z3); %a3 = h_theta = z3
 % h_theta
 
 
 % funçao de custo
-J = (1/m) * sum ( sum (  (-Y)' .* log(h_theta) -  (1-Y)' .* log(1-h_theta) )); % nao está funcionando
+%J = (1/m) * sum ( sum (  (-Y)' .* log(h_theta) -  (1-Y)' .* log(1-h_theta) )); % nao está funcionando
 
+%Forward Propagation + Calculo de Custo
 
+a1 = X;
+z2 = a1 * theta_1';
+a2 = sigmoid(z2);
+a2 = [ones(m , 1)  a2];
+a3 = sigmoid(a2 * theta_2');
+
+ident = eye(num_labels)(Y,:);
+
+custo = ident.*log(a3) + (1 - ident).*log(1 - a3);
+J = -sum(sum(custo,2)) / m;
+
+reg = sum(sum(theta_1(:,2:end).^2)) + sum(sum(theta_2(: , 2:end).^2));
+
+J = J + lambda/(2*m)*reg;
+
+%
+
+%backpropagation
+
+delta3 = a3 - ident;
+delta2 = (delta3*theta_2)(:,2:end) .* sigmoidGradiente(z2);
+
+Delta1 = delta2'*a1;
+Delta2 = delta3'*a2;
+
+Theta1_grad = Delta1 / m + lambda*[zeros(hidden_layer_size , 1) theta_1(:,2:end)] / m;
+Theta2_grad = Delta2 / m + lambda*[zeros(num_labels , 1) theta_2(:,2:end)] / m;
+
+%
+
+res = [Theta1_grad(:), Theta2_grad(:)];
 
 %% Finalizacao
 %clear; close all;
