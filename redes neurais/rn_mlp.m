5ab1253d6869b593d8cd0195858f7806bb14182b
thamiluz <thamires@MacBook-Pro-de-Thamires.local>
2013-11-13 00:17:53 -0200

    Alteracao dos theta, camada de saida e funcao custo

--- redes neurais/rn_mlp.m
+++ redes neurais/rn_mlp.m
@@ -12,7 +12,7 @@ Y = data(:, end );
 
 %% Seta parametros
 tam_cam_entrada  = 137; % quantidade de neuronios na camada de entrada 
-tam_cam_saida  = 2;     % quantidade de neuronios na camada de saida
+tam_cam_saida  = 1;     % quantidade de neuronios na camada de saida
 tam_cam_inter = 274;    % quantidade de neuronios na camada intermediaria
 num_cam = 3;            % quantidades de camadas   
 lambda = 0;
@@ -25,8 +25,12 @@ lambda = 0;
 X = [ones(m, 1) x];
  
 %gera valores randomicos entre 0 e 1 para os thetas
-theta_1 = rand(n + 1, tam_cam_inter);
-theta_2 = rand(tam_cam_inter + 1, tam_cam_saida); 
+%theta_1 = rand(n + 1, tam_cam_inter);
+theta_1 = 1 + (-1-1).*rand(n + 1, tam_cam_inter);
+theta_2 = 1 + (-1-1).*rand(tam_cam_inter + 1, tam_cam_saida); 
+
+%theta_1 = rand(n + 1, 1);
+%theta_2 = rand(tam_cam_inter + 1, 2); 
 
 J = 0;
 Theta1_grad = zeros(size(theta_1));
@@ -48,21 +52,38 @@ Theta2_grad = zeros(size(theta_2));
 %J = (1/m) * sum ( sum (  (-Y)' .* log(h_theta) -  (1-Y)' .* log(1-h_theta) )); % nao está funcionando
 
 %Forward Propagation + Calculo de Custo
-
+size(theta_1)
+size(theta_2)
 a1 = X;
-z2 = a1 * theta_1';
+z2 = a1 * theta_1;
 a2 = sigmoid(z2);
 a2 = [ones(m , 1)  a2];
-a3 = sigmoid(a2 * theta_2');
+z3 = a2 * theta_2;
+a3 = sigmoid(z3);
 
-ident = eye(num_labels)(Y,:);
+ident = eye(num_cam);%(Y,:);
+%ident = eye(num_labels);
 
-custo = ident.*log(a3) + (1 - ident).*log(1 - a3);
-J = -sum(sum(custo,2)) / m;
+%custo = ident.*log(a3) + (1 - ident).*log(1 - a3);
+%J = -sum(sum(custo,2)) / m;
 
-reg = sum(sum(theta_1(:,2:end).^2)) + sum(sum(theta_2(: , 2:end).^2));
+%reg = sum(sum(theta_1(:,2:end).^2)) + sum(sum(theta_2(: , 2:end).^2));
 
-J = J + lambda/(2*m)*reg;
+%J = J + lambda/(2*m)*reg;
+
+% Voce precisa retornar as seguintes variaveis corretamente
+J = 0;
+% Initializa variaveis
+lambda = 1;
+
+y = Y;
+
+theta_3 = theta_2;
+theta_3(1) = 0;
+reg_c = (lambda  * sum(  theta_2(2:size(theta_2)) .^2 ) )/ (2 * m);
+J = (sum( -y.*(log( a3 ) ) - (( 1 .- y) .* log( 1 .- a3 ) )  ) / m )  ;
+
+J
 
 %
 
