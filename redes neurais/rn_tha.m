280c85d6cb23b0702a4f75603d56a4b730304849
thamiluz <thamires@MacBook-Pro-de-Thamires.local>
2013-11-13 16:46:04 -0200

    teste

--- redes neurais/rn_tha.m
+++ redes neurais/rn_tha.m
@@ -8,12 +8,16 @@ data = load('base.txt');
 % Separa os atribuos na matriz X, e Y
 x = data(:, 1:end-1 ); 
 Y = data(:, end );
+%load('base_rl.mat');
 
-
+%x = X;
+%Y = y;
 %% Seta parametros
-tam_cam_entrada  = 137; % quantidade de neuronios na camada de entrada 
-tam_cam_saida  = 1;     % quantidade de neuronios na camada de saida
-tam_cam_inter = 274;    % quantidade de neuronios na camada intermediaria
+%tam_cam_entrada  = 137; % quantidade de neuronios na camada de entrada 
+tam_cam_entrada  = size(x,2) % quantidade de neuronios na camada de entrada 
+tam_cam_saida    = 1;     % quantidade de neuronios na camada de saida
+%tam_cam_inter = 274;    % quantidade de neuronios na camada intermediaria
+tam_cam_inter = tam_cam_entrada * 2    % quantidade de neuronios na camada intermediaria
 num_cam = 3;            % quantidades de camadas   
 lambda = 0;
                           
@@ -27,12 +31,27 @@ X = [ones(m, 1) x];
 %gera valores randomicos entre 0 e 1 para os thetas
 theta_1 = 1 + (-1-1).*rand(n + 1, tam_cam_inter);
 theta_2 = 1 + (-1-1).*rand(tam_cam_inter + 1, tam_cam_saida); 
-theta_inicial = [theta_1(:)' theta_2(:)'];
-size(theta_inicial)
-opcoes = optimset('GradObj', 'on');
-[theta, custo] = ...
-  fminunc(@(t)(JDeltha(t,X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida)), theta_inicial);
+tam_t1 = size(theta_1)
+tam_t2 = size(theta_2)
+theta_inicial = [theta_1(:) ; theta_2(:)];
+%theta_inicial = [theta_1(:)' theta_2(:)'];
+tam_theta_ini = size(theta_inicial)
+opcoes = optimset('GradObj', 'on', 'MaxIter', '100');
+%[custo, grad] = JDeltha(theta_inicial, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida);
 
+%custo = 0;
+%grad =0;
+%[custo, grad] = JDeltha(theta_inicial, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida);
+
+%[custo, grad] = JDeltha(grad, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida);
+
+%[custo, grad] = JDeltha(grad, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida);
+%[custo, grad] = JDeltha(grad, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida);
+%[custo, grad] = JDeltha(grad, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida);
+
+[theta, custo] = ...
+  fsolve(@(t)(JDeltha(t, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida)), theta_inicial, opcoes);
+custo
 %classificar
 
 m = size(X, 1); % Tamanho das amostras de X
