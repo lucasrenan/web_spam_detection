dab6676f8fe7f7b23d027649c27960da0570de59
thamiluz <thamires@MacBook-Pro-de-Thamires.local>
2013-11-13 23:39:27 -0200

    teste

--- redes neurais/JDeltha.m
+++ redes neurais/JDeltha.m
@@ -1,16 +1,19 @@
-function [J, grad] = JDeltha(theta, X, y,tam_cam_entrada, tam_cam_inter,num_cam)
+function [J, grad] = JDeltha(theta, X, y, tam_cam_entrada, tam_cam_inter, tam_cam_saida)
   
- [m, n] = size(X);
+[m, n] = size(X);
+lambda = 1;
 
-theta_1 = reshape( theta(1:tam_cam_inter * (tam_cam_entrada + 1)), (tam_cam_entrada + 1),tam_cam_inter );
+theta_1 = reshape( theta(1:tam_cam_inter * (tam_cam_entrada + 1)), (tam_cam_entrada + 1), tam_cam_inter );
+theta_2 = reshape( theta((1 + (tam_cam_inter * (tam_cam_entrada + 1))):end), (tam_cam_inter + 1) , tam_cam_saida);
+
+% ================ Printa valores
 valor_theta1 = theta_1(1,2)
-theta_2 = reshape( theta((1 + (tam_cam_inter * (tam_cam_entrada + 1))):end), (tam_cam_inter + 1) , num_cam);
 valor_theta2 = theta_2(1,1)
 
 tam_theta1 = size(theta_1)
 tam_theta2 = size(theta_2)
 
-%Forward Propagation + Calculo de Custo
+%================= Forward Propagation + Calculo de Custo
 a1 = [ones(m , 1)  X];
 z2 = a1 * theta_1;
 a2 = sigmoid(z2);
@@ -18,48 +21,39 @@ a2 = [ones(m , 1)  a2];
 z3 = a2 * theta_2;
 a3 = sigmoid(z3);
 
-% Initializa variaveis
-lambda = 1;
-
-reg_c = (lambda  * sum(  theta_2(2:size(theta_2)) .^2 ) )/ (2 * m);
+reg_c = (lambda  * sum( theta_1(2:end) .^2 ) )/ (2 * m);
+reg_c = reg_c + (lambda  * sum( theta_2(2:end) .^2 ) )/ (2 * m);
 J = (sum( -y.*(log( a3 ) ) - (( 1 .- y) .* log( 1 .- a3 ) )  ) / m ) + reg_c ;
 
-%backpropagation
+%================= Back Propagation 
 
 d3 = a3 - y;
-d2 = (d3*theta_2').* (a2.* (1-a2));%sigmoidGradiente(z2);
+d2 = (d3*theta_2').* (a2.* (1-a2));
 
 deltha1 = a1' * d2;
 deltha2 = a2' * d3;
 
-%Theta1_grad = Delta1 / m + lambda*[zeros(hidden_layer_size , 1) Theta1(:,2:end)] / m;
-%Theta2_grad = Delta2 / m + lambda*[zeros(num_labels , 1) Theta2(:,2:end)] / m;
+%=================Printa valores
+tamd3 = size(d3);
+tamd2 = size(d2);
+tamdel1 = size(deltha1);
+tamdel2 = size(deltha2);
 
 theta_c_1 = theta_1;
-theta_c_1 = [zeros(size(theta_1,1) , 1) theta_c_1];
+theta_c_1(1) = 0;
 
 theta_c_2 = theta_2;
 theta_c_2(1) = 0;
 
-D_1 = (deltha1/m) + (lambda * theta_c_1); 
-
-D_1 = D_1(:,2:end);
+D_1 = (deltha1(:,2:end)/m) + (lambda * theta_c_1); 
 D_2 = (deltha2/m) + (lambda * theta_c_2); 
 
-%tam_d1 = size(D_1)
-%tam_d2 = size(D_2)
-%delta_unico = [D_1(:) ; D_2(:)];
-%tam_delta_unico = size(delta_unico)
-%teste1 = reshape( delta_unico(1:tam_cam_inter * (tam_cam_entrada + 1)), (tam_cam_entrada + 1),tam_cam_inter );
-%teste2 = reshape( delta_unico((1 + (tam_cam_inter * (tam_cam_entrada + 1))):end), (tam_cam_inter + 1) , num_cam);
-%size(teste1)
-%size(teste2)
-delta_unico = [D_1(:)' D_2(:)' ];
+%=================Printa valores
+tamD1 = size(D_1)
+tamD2 = size(D_2)
 
-%custo = J;
-grad = delta_unico;
+grad = [D_1(:) ; D_2(:)];
+%grad = [D_1(:)' D_2(:)'];
 J
-%size(delta)
-%size(delta)
 
 end
--- redes neurais/rn_tha.m
+++ redes neurais/rn_tha.m
@@ -5,49 +5,56 @@ fprintf('Carregando os dados...\n\n');
 
 % Carrega os dados da base
 data = load('base.txt');
-% Separa os atribuos na matriz X, e Y
-x = data(:, 1:end-1 ); 
-Y = data(:, end );
-%load('base_rl.mat');
+%X = data(:, 1:end-1 ); 
+%Y = data(:, end );
+load('base_rl.mat');
 
-X = x;
-%Y = y;
+%X = x;
+Y=y;
+[m, n] = size(X);
+m
+n
 %% Seta parametros
-%tam_cam_entrada  = 137; % quantidade de neuronios na camada de entrada 
-tam_cam_entrada  = size(x,2); % quantidade de neuronios na camada de entrada 
-tam_cam_saida    = 1;     % quantidade de neuronios na camada de saida
-%tam_cam_inter = 274;    % quantidade de neuronios na camada intermediaria
-tam_cam_inter = tam_cam_entrada * 2;    % quantidade de neuronios na camada intermediaria
-num_cam = 3;            % quantidades de camadas   
+tam_cam_entrada  = n;                    % quantidade de neuronios na camada de entrada 
+tam_cam_saida    = 1;                    % quantidade de neuronios na camada de saida
+tam_cam_inter    = tam_cam_entrada * 2;  % quantidade de neuronios na camada intermediaria
+num_cam          = 3;                    % quantidades de camadas   
 lambda = 0;
                           
-
-%  Configura a matriz apropriadamente e adiciona uns na primeira coluna
-[m, n] = size(x);
-
-%adiciona coluna de 1's na matriz x
-%X = [ones(m, 1) x];
  
-%gera valores randomicos entre 0 e 1 para os thetas
-theta_1 = 1 + (-1-1).*rand(n + 1, tam_cam_inter);
-theta_2 = 1 + (-1-1).*rand(tam_cam_inter + 1, tam_cam_saida); 
-tam_t1 = size(theta_1)
-tam_t2 = size(theta_2)
-%theta_inicial = [theta_1(:) ; theta_2(:)];
-theta_inicial = [theta_1(:)' theta_2(:)'];
-tam_theta_ini = size(theta_inicial)
+%Inicialização do theta inicial com valores proximos de zero
+theta_1 = rand(n + 1, tam_cam_inter);
+theta_2 = rand(tam_cam_inter + 1, tam_cam_saida); 
+
+theta_inicial = [theta_1(:) ; theta_2(:)];
+%theta_inicial = [theta_1(:)' theta_2(:)']
+
+
+%================================= Funcao FMIUNC ========================================
 opcoes = optimset('GradObj', 'on', 'MaxIter', '100');
 
 [theta, custo] = ...
-  fminunc(@(t)(JDeltha(t, X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida)), theta_inicial, opcoes);
+  fminunc(@(t)(JDeltha(t,X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida)), theta_inicial, opcoes);
+
 custo
 %classificar
 
-m = size(X, 1); % Tamanho das amostras de X
+theta_1 = reshape( theta(1:tam_cam_inter * (tam_cam_entrada + 1)), (tam_cam_entrada + 1), tam_cam_inter );
+theta_2 = reshape( theta((1 + (tam_cam_inter * (tam_cam_entrada + 1))):end), (tam_cam_inter + 1) , tam_cam_saida);
+
+a1 = [ones(m , 1)  X];
+z2 = a1 * theta_1;
+a2 = sigmoid(z2);
+a2 = [ones(m , 1)  a2];
+z3 = a2 * theta_2;
+a3 = sigmoid(z3);
+
+m = size(a3, 1); % Tamanho das amostras de X
 p = zeros(m, 1);
 
 % Calcular a sigmoidal
-sig = sigmoid( X * theta );
+%sig = sigmoid( X * theta );
+sig = a3;
 
 % Classificar as amostras
 for i=1:m 
