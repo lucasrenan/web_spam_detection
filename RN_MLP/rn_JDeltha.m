fece4f04b7524a747f6bd71c37a2ae3b12a18db8
thamiluz <thamires@mbp-de-thamires.lan>
2013-11-16 15:55:56 -0200

    Codigos comentados

--- RN_MLP/rn_JDeltha.m
+++ RN_MLP/rn_JDeltha.m
@@ -1,19 +1,36 @@
-function [J, grad] = rn_JDeltha(theta, X, y, tam_cam_entrada, tam_cam_inter, tam_cam_saida)
+function [J, theta] = rn_JDeltha(theta, X, y, tam_cam_entrada, tam_cam_inter, tam_cam_saida)
+% Função rn_JDeltha
+% 	Calcula o custo e os thetas para serem otimizados.
+%
+% Entrada:
+%  theta           = theta das camadas concatenados
+%  X               = Matriz com os atributos e amostras da base
+%  y               = Matriz com as classes da base
+%  tam_cam_entrada = Quantidade de neuronios da camada de entrada
+%  tam_cam_inter   = Quantidade de neuronios da camada intermediaria
+%  tam_cam_saida   = Quantidade de neuronios  da camada de saida
+%
+% Saída:
+%   J     = Custo
+%   theta = theta das camadas concatenados e atualizados
+% ============================================================
   
+%Inicializar variaveis
 [m, n] = size(X);
 lambda = 0.8;
 
+%Separar os thetas das camadas
 theta_1 = reshape( theta(1:tam_cam_inter * (tam_cam_entrada + 1)), (tam_cam_entrada + 1), tam_cam_inter );
 theta_2 = reshape( theta((1 + (tam_cam_inter * (tam_cam_entrada + 1))):end), (tam_cam_inter + 1) , tam_cam_saida);
 
 %================= Forward Propagation + Calculo de Custo
+%Adiciona o bias, e calcula a sigmoidal
 a1 = [ones(m , 1) X];
-z2 = a1 * theta_1;
-a2 = sigmoid(z2);
-a2 = [ones(m , 1) a2];
-z3 = a2 * theta_2;
-a3 = sigmoid(z3);
+a2 = sigmoid(a1 * theta_1);
+a2 = [ones(m , 1) a2]; 
+a3 = sigmoid(a2 * theta_2);
 
+%Calcula o custo
 reg_c = (lambda  * sum( theta_1(2:end) .^2 ) )/ (2 * m);
 reg_c = reg_c + (lambda  * sum( theta_2(2:end) .^2 ) )/ (2 * m);
 
@@ -21,7 +38,7 @@ b = (( 1 .- y) .* log( 1 .- a3 ) );
 J = sum(sum( -y.*(log( a3 ) ) - b ) / m ) + reg_c ;
 
 %================= Back Propagation 
-
+% Calcula os erros e propaga
 d3 = (a3 - y);
 d2 = d3 * theta_2'.* (a2.*(1-a2));
 d2(:,1) = [];%remove o bias
@@ -29,7 +46,7 @@ d2(:,1) = [];%remove o bias
 deltha1 = a1' * d2;
 deltha2 = a2' * d3;
 
-% ================ Calcula novo theta
+% ================ Calcula os novos thetas
 
 D_1 =   lambda * deltha1; 
 D_2 =   lambda * deltha2;
@@ -37,7 +54,9 @@ D_2 =   lambda * deltha2;
 theta_1 = D_1 - theta_1;
 theta_2 = D_2 - theta_2;
 
+% Concatena os thetas das camadas
+theta = [theta_1(:) ; theta_2(:)];
 
-grad = [theta_1(:) ; theta_2(:)];
+% ==================================================================
 
 end
--- RN_MLP/rn_mlp.m
+++ RN_MLP/rn_mlp.m
@@ -15,6 +15,7 @@ data = load('base.txt');
 X = data(:, 1:end-1 ); 
 Y = data(:, end );
 
+%------------------- Inicializar variaveis -----------------------------
 %Normalizar a base
 X = normalizarAtributos(X);
 
@@ -22,50 +23,57 @@ X = normalizarAtributos(X);
 
 %% Inicializar Parâmetros
 tam_cam_entrada  = n;  % quantidade de neuronios na camada de entrada 
-tam_cam_saida    = 1;  % quantidade de neuronios na camada de saida
 tam_cam_inter    = 20; % quantidade de neuronios na camada intermediaria
-                          
+tam_cam_saida    = 1;  % quantidade de neuronios na camada de saida
  
-%Inicialização do theta inicial com valores proximos de zero
+%Carregar thetas predefinidos para a base de dados
+theta_inicial = load('theta_ini');
+
+%Caso deseje, os thetas podem ser carregados de forma randomica, descomente  o código abaixo
 %theta_1 = 0.1*(1-2*rand(tam_cam_entrada+1,tam_cam_inter));
 %theta_2 = 0.1*(1-2*rand(tam_cam_inter+1,tam_cam_saida));
 %theta_inicial = [theta_1(:) ; theta_2(:)];
-theta_inicial = load('theta_ini');
 
-%================================= Funcao FMINUNC ========================================
+
+%------------------- Calcular o theta otimo ------------------------------
+disp('Calcular o theta ótimo.');
+fflush(stdout);
+%Encontrar o theta otimo
 op = optimset('GradObj', 'on');
-
 [theta, custo] = ...
   fminunc(@(t)(rn_JDeltha(t,X, Y, tam_cam_entrada, tam_cam_inter,tam_cam_saida)), theta_inicial, op);
 
 
-%================================= Classificar
+%------------------- Classificar as amostras -----------------------------
 
+disp('Classificar as amostras.');
+fflush(stdout);
+%Separar os thetas das camadas
 theta_1 = reshape( theta(1:tam_cam_inter * (tam_cam_entrada + 1)), (tam_cam_entrada + 1), tam_cam_inter );
 theta_2 = reshape( theta((1 + (tam_cam_inter * (tam_cam_entrada + 1))):end), (tam_cam_inter + 1) , tam_cam_saida);
 
-a1 = [ones(m , 1)  X];
-a2 = sigmoid(a1 * theta_1);
-a2 = [ones(m , 1)  a2];
-a3 = sigmoid(a2 * theta_2);
+%Classificar
+p = rn_predicao(theta_1, theta_2, X);
 
-m = size(a3, 1); 
-p = zeros(m, 1);
-
-% Calcular a sigmoidal
-media = 0.5;
-sig = a3;
-
-% Classificar as amostras
-for i=1:m 
-  if ( sig(i)  >= media)
-    p(i) = 1;
-  else
-    p(i) = 0;
-  end
-end;
-p;
-ac = (sum(Y == p)/size(Y,1)) * 100;
+%------------------- Exibir resultados ---------------------------------
+fprintf('Resultados Obtidos com as Redes Neurais Artificiais - MultiLayer Perceptron:\n');
+fflush(stdout);
+%Acuracia do algoritmo
+ac = (sum(Y == p)/size(p,1)) * 100;
 fprintf('Acuracia: %f\n', ac);
-%% Finalizacao
-%clear; close all;
+
+% Encontra os valores de TP, FP e FN para calculos
+[TP, FP, FN] = valoresTblConfusao(Y, p);
+
+%precisao
+pcs = (TP/(TP+FP)) * 100;
+fprintf('Precisao: %f\n', pcs);
+
+%revocacao
+rev = (TP/(TP+FN)) * 100;
+fprintf('Revocacao: %f\n', rev);
+
+%f-medida
+fmed = 2 * (rev*pcs)/(rev+pcs);  
+fprintf('F-Medida: %f\n', fmed);
+
